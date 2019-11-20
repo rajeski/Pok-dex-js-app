f578f57d1f2e89c7eb183aada7db936a134906f6
rajeski <asiajsr@gmail.com>
2019-11-20 15:59:28 +0900

    Update scripts.js
    Removed Task 1.1's (JavaScript) code. For (JavaScript) Task 2.2: created a new var (repository); added three Pokemon keys (name, height and types).

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-var favoriteFood = 'See-Food! ;-)';
-document.write(favoriteFood);
+var repository = [ {name: 'Beedrill', height: 1.0, types: ['swarm', 'sniper'] },
+    {name: 'Charmeleon', height: 1.1, types: ['blaze', 'solar power'] },
+    {name: 'Fearow', height: 1.2, types: ['keen-eye', 'sniper'] },
+    {name: 'Pickachu', height: 0.4, types: ['lightningrod', 'static'] } ];
