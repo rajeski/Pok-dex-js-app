69afa621e78bb15606285f67b1429d1620879e7d
rajeski <asiajsr@gmail.com>
2019-11-22 23:55:30 +0900

    Updated scripts.js
    Changed the for loop to a for.Each loop.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,5 @@ var repository = [ {name: 'Beedrill', height: 1.0, types: ['swarm', 'sniper'] },
     {name: 'Fearow', height: 1.2, types: ['keen-eye', 'sniper'] },
     {name: 'Pickachu', height: 0.4, types: ['lightningrod', 'static'] } ];
 
-for (let i = 0; i < repository.length; i++) {
-  var pokemon = repository[i];
-  if (pokemon.height > 0.5) {
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that's a big pocket monster!</p>");
-  } else {
-    document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>");
-  }
-} 
+    repository.forEach({name, height, types}) 
+    console.log(name, height, types);
