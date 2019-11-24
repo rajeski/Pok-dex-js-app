f2bba118cb614ec5847f34b37f22db51e95cf118
rajeski <asiajsr@gmail.com>
2019-11-24 11:09:56 +0900

    Updated scripts.js
    Wrapped var repository in IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
-var repository = [ {name: 'Beedrill', height: 1.0, types: ['swarm', 'sniper'] },
-    {name: 'Charmeleon', height: 1.1, types: ['blaze', 'solar power'] },
-    {name: 'Fearow', height: 1.2, types: ['keen-eye', 'sniper'] },
-    {name: 'Pickachu', height: 0.4, types: ['lightningrod', 'static'] } ];
-
-    repository.forEach(function(arrayElement){
-        return console.log(arrayElement);
+var pokemonRepository = (function () {
+    var repository = [ {name: 'Beedrill', height: 1.0, types: ['swarm', 'sniper'] },
+        {name: 'Charmeleon', height: 1.1, types: ['blaze', 'solar power'] },
+        {name: 'Fearow', height: 1.2, types: ['keen-eye', 'sniper'] },
+        {name: 'Pickachu', height: 0.4, types: ['lightningrod', 'static'] } ];
+    
+        function add(item) {
+            return repository.push(item);
+          }
+        
+          function getAll() {
+            return repository;
+          }
+          return {
+            add: add,
+            getAll: getAll
+          };
+        })();
+        
+        pokemonRepository.getAll().forEach(pokemon => {
+            if (pokemon.height > 0.5) {
+                document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ") - Wow, that is a big pocket monster!</p>");
+              } else {
+                document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")</p>");
+              }
         });
